fd5e36d1663a8c1701a8b8ead65de4f2b2898d18
sammitchell <mitchesama1@myvuw.ac.nz>
2022-01-11 11:26:04 +1300

    Initial commit

---
+++ src/main/java/test_mod/items/ItemBase.java
@@ -0,0 +1,9 @@
+package test_mod.items;
+
+import net.minecraft.world.item.Item;
+
+public class ItemBase extends Item {
+    public ItemBase() {
+        super(new Item.Properties().group(ItemG));
+    }
+}
